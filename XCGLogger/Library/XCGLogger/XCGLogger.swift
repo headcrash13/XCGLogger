297db180d75fa33bb421e7bf537073d3562fa36e
Glenn Sugden <headcrash@mac.com>
2015-06-19 14:21:07 -0700

    Added #if os(watchOS) for UIKit
    Added #if os(iOS) || os(watchOS) for UIKit specific conditional
    compilation

--- XCGLogger/Library/XCGLogger/XCGLogger.swift
+++ XCGLogger/Library/XCGLogger/XCGLogger.swift
@@ -8,7 +8,7 @@
 //
 
 import Foundation
-#if os(iOS)
+#if os(iOS) || os(watchOS)
     import UIKit
 #endif
 
@@ -366,7 +366,7 @@ public class XCGLogger: CustomDebugStringConvertible {
             self.bg = bg
         }
 
-#if os(iOS)
+#if os(iOS) || os(watchOS)
         public init(fg: UIColor, bg: UIColor? = nil) {
             var redComponent: CGFloat = 0
             var greenComponent: CGFloat = 0
